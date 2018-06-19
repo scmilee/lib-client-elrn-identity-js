13221f35f0bf4c060be68664ac080dcac48e7de9
scmilee <drudged@oregonstate.edu>
2018-06-19 14:24:25 -0700

    add wrapper file for blockstack redirectToSignIN function

---
+++ src/redirectToSignIn.js
@@ -0,0 +1,11 @@
+const blockstack = require('blockstack')
+
+export default (redirectURI, manifestURI, permissionsArray) => {
+    return new Promise((resolve, reject) => {
+        try{
+        	resolve(blockstack.redirectToSignIn(redirectURI, manifestURI, permissionsArray));
+        } catch(err) {
+        	reject(err);
+        }
+    });
+}s
